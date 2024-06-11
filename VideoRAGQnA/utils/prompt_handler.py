28e52556465cfe452f84cecaf47ea22b497dce1e
VoidZeroe <93091061+VoidZeroe@users.noreply.github.com>
2024-06-11 11:48:53 +0100

    Update prompt_handler.py

--- VideoRAGQnA/utils/prompt_handler.py
+++ VideoRAGQnA/utils/prompt_handler.py
@@ -3,6 +3,12 @@ from jinja2 import Environment, BaseLoader
 PROMPT = open("utils/prompt_template.jinja2").read().strip()
 
 def get_formatted_prompt(scene, prompt, history):
+    newline = "\n"
+    # formated = f"{newline}User: {history[0]}{newline}Assistant: {history[1]}{newline}"
+    try:
+        formated = "\n[INST]\n".join(history)
+    except:
+        formated = "\n[INST]\n".join(["hello", "hi"])
     env = Environment(loader=BaseLoader())
     template = env.from_string(PROMPT)
-    return template.render(scene=scene, prompt=prompt, history=history)
+    return template.render(scene=scene, prompt=prompt, history=formated)
