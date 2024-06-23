0ed309ce0a1de1d1ad4545b26465c9acb6539882
VoidZeroe <93091061+VoidZeroe@users.noreply.github.com>
2024-06-23 08:55:15 +0100

    Update video-rag-ui.py

--- VideoRAGQnA/video-rag-ui.py
+++ VideoRAGQnA/video-rag-ui.py
@@ -130,17 +130,17 @@ class VideoLLM(LLM):
         chat.upload_video_without_audio(video_path, start_time, duration)
         chat.ask(text_input)#, chat_state)
         #answer = chat.answer(chat_state, img_list, max_new_tokens=300, num_beams=1, min_length=1, top_p=0.9, repetition_penalty=1.0, length_penalty=1, temperature=0.1, max_length=2000, keep_conv_hist=True, streamer=streamer)
-        answer = chat.answer(max_new_tokens=300, num_beams=1, min_length=1, top_p=0.9, repetition_penalty=1.0, length_penalty=1, temperature=0.1, max_length=2000, keep_conv_hist=True, streamer=streamer)
+        self.answer = chat.answer(max_new_tokens=300, num_beams=1, min_length=1, top_p=0.9, repetition_penalty=1.0, length_penalty=1, temperature=0.1, max_length=2000, keep_conv_hist=True)#, streamer=streamer)
         print("-"*30)
         print("vlm call successful")
     def stream_res(self, video_path, text_input, chat, start_time, duration):
         print("-"*30)
         print("starting model stream")
-        #thread = threading.Thread(target=self._call, args=(video_path, text_input, chat, chat_state, img_list, streamer))  # Pass streamer to _call
-        thread = threading.Thread(target=self._call, args=(video_path, "<rag_prompt>"+text_input, chat, start_time, duration, streamer))  # Pass streamer to _call
-        thread.start()
+        # #thread = threading.Thread(target=self._call, args=(video_path, text_input, chat, chat_state, img_list, streamer))  # Pass streamer to _call
+        # thread = threading.Thread(target=self._call, args=(video_path, "<rag_prompt>"+text_input, chat, start_time, duration, streamer))  # Pass streamer to _call
+        # thread.start()
         
-        for text in streamer:
+        for text in self.answer:
             yield text
         print("-"*30)
         print("streaming model done")
